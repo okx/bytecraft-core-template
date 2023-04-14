52593fa3cbd47ae36c66985079ea757d180e841d
Smea1 <1476977430@qq.com>
2023-04-14 15:46:30 +0800

    first update

--- keys.js
+++ keys.js
@@ -6,6 +6,7 @@ module.exports = {
         mnemonic: "puzzle glide follow cruel say burst deliver wild tragic galaxy lumber offer",
     },
 
+    
     alice: {
         privateKey: "43792143508f053a8b82dd83e1d56c82dc04cd0fcc86220175ef591911fa65c1",
     },
