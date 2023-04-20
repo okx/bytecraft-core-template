3e1178f80fb361b31b0bdc8833e4df1deda43add
f <fan.ke@okg.com>
2023-04-20 15:11:08 +0800

    support syliva

--- tasks/example-custom-logic.ts
+++
@@ -1,5 +0,0 @@
-import { Env, task } from "@okexchain/bytecraft";
-
-task(async (_env: Env) => {
-
-});
--- tasks/template.ts
+++ tasks/template.ts
@@ -1,6 +1,8 @@
 import { Env, task } from "@okexchain/bytecraft";
 
 task(async (env:Env) => {
-  console.log(env);
-  console.log("Template")
+  await env.client.execute(env.client.refs.mydapp.contractAddresses.default, env.defaultWallet, {
+    freeze: {},
+  });
+  console.log("freeze done!")
 });
