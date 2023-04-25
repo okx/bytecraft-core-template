712c29d860ec0f781fdc9476df50e8f93be141ac
f <fan.ke@okg.com>
2023-04-25 16:26:42 +0800

    update version

--- tasks/template.ts
+++ tasks/template.ts
@@ -3,6 +3,6 @@ import { Env, task } from "@okexchain/bytecraft";
 task(async (env:Env) => {
   await env.client.execute(env.client.refs.mydapp.contractAddresses.default, env.defaultWallet, {
     freeze: {},
-  });
+  },'auto');
   console.log("freeze done!")
 });
